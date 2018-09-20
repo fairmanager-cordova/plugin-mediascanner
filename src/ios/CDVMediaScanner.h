132681b67889792ee726a97a1f3a0d1216f68e18
Oliver Salzburg <oliver.salzburg@gmail.com>
2018-09-20 16:12:21 +0200

    [FIX] Code style
    ...although it's not even possible to write ObjC that looks good.

--- src/ios/CDVMediaScanner.h
+++ src/ios/CDVMediaScanner.h
@@ -2,6 +2,10 @@
 
 @interface CDVMediaScanner : CDVPlugin {}
 
-- (void)insertImage:(UIImage *)image intoAlbumNamed:(NSString *)albumName;
+- (void)				insertImage:						(UIImage*)				image
+						intoAlbumNamed:						(NSString*)				albumName;
+- (PHAssetCollection*)	fetchAssetCollectionWithAlbumName:	(NSString*)				albumName;
+- (void)				insertImage:						(UIImage*)				image
+						intoAssetCollection:				(PHAssetCollection*)	collection
 
 @end
